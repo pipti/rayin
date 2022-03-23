01cac6f306b67c54014aacdc4f71aeb0a5fd097a
wz <pipti@163.com>
2022-03-24 00:29:31 +0800

    修改springboot样例

--- rayin-springboot-server/src/main/java/ink/rayin/springboot/controller/PDFCreatorController.java
+++ rayin-springboot-server/src/main/java/ink/rayin/springboot/controller/PDFCreatorController.java
@@ -2,10 +2,16 @@ package ink.rayin.springboot.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import ink.rayin.htmladapter.base.PdfGenerator;
+import ink.rayin.tools.utils.Charsets;
+import ink.rayin.tools.utils.ResourceUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 
 @Slf4j
 @RestController
@@ -18,13 +24,33 @@ public class PDFCreatorController {
     /**
      * 项目模板生成接口
      *
-     * @param tplNo
+     * @param tplName
      * @param jsonData
      * @return
      */
-    @GetMapping(value = "/pdf/createbytplno", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void pdfCreateByTplNo(@PathVariable("tplNo") String tplNo, @RequestBody JSONObject jsonData) throws Exception {
-        pdfGenerator.generatePdfFileByTplConfigFile("tpl/"+tplNo , jsonData, "./tmp/a.pdf");
+    @GetMapping(value = "/pdf/create/tpl/{tplName}/file", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void pdfCreateByTplToFile(@PathVariable("tplName") String tplName, @RequestBody JSONObject jsonData) throws Exception {
+        pdfGenerator.generatePdfFileByTplConfigFile("tpl/"+tplName + "/tpl.json" , jsonData, "./tmp/a.pdf");
     }
+
+
+    /**
+     * 项目模板生成接口
+     *
+     * @param tplName
+     * @param jsonData
+     * @return
+     */
+    @PostMapping(value = "/pdf/create/tpl/{tplName}/os")
+    public void pdfCreateByTplToOS(@PathVariable("tplName") String tplName, @RequestBody JSONObject jsonData, HttpServletResponse response) throws Exception {
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        pdfGenerator.generatePdfStreamByTplConfigStr(ResourceUtil.getResourceAsString("tpl/"+tplName + "/tpl.json", Charsets.UTF_8) , jsonData, bao);
+        response.setContentType("application/pdf;charset=utf-8");
+        OutputStream stream = response.getOutputStream();
+        stream.write(bao.toByteArray());
+        stream.flush();
+        stream.close();
+    }
+
 
 }
