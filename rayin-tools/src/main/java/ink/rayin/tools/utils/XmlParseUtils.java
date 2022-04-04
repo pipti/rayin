8185f2124ff91c451c55cb8784a5096d0eb0f1c6
wz <pipti@163.com>
2022-04-04 12:37:33 +0800

    新增xml处理类

---
+++ rayin-tools/src/main/java/ink/rayin/tools/utils/XmlParseUtils.java
@@ -0,0 +1,41 @@
+package ink.rayin.tools.utils;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class XmlParseUtils {
+
+    public static Object xmlStrToObject(String xmlStr, Class clazz) {
+        Object xmlObject = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(clazz);
+            // 进行将Xml转成对象的核心接口
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            StringReader sr = new StringReader(xmlStr);
+            xmlObject = unmarshaller.unmarshal(sr);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return xmlObject;
+    }
+
+    public static String objectToXmlStr(Object obj,Class<?> load){
+        String result = "";
+        try{
+            JAXBContext context = JAXBContext.newInstance(load);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+            StringWriter writer = new StringWriter();
+            marshaller.marshal(obj,writer);
+            result = writer.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
