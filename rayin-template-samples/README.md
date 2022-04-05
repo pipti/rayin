03500366e6572f6d59679ff1c168b767c13859ba
wz <pipti@163.com>
2022-04-05 15:05:32 +0800

    新增template-samples模块

---
+++ rayin-template-samples/README.md
@@ -0,0 +1,12 @@
+# 行业模板样例
+说明：共享行业模板样例，方便定制使用。
+
+## 文件名规范
+data.json : 数据
+data_desc.json : 数据说明
+element1.html : 构件
+tpl.json : 模板
+字体文件单独放入fonts中
+
+测试程序默认查找所有tpl.json，对应目录下的data.json
+生成目录至tmp目录下
---
+++ rayin-template-samples/src/test/java/ink/rayin/samples/PdfBoxGeneratorOpenhtmltopdfSamples.java
@@ -0,0 +1,86 @@
+package ink.rayin.samples;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import ink.rayin.htmladapter.base.PdfGenerator;
+import ink.rayin.htmladapter.base.utils.JsonSchemaValidator;
+import ink.rayin.htmladapter.openhtmltopdf.service.PdfBoxGenerator;
+import ink.rayin.tools.utils.FileUtil;
+import ink.rayin.tools.utils.ResourceUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * Openhtmltopdf适配生成器行业模板样例
+ * Openhtmltopdf adapter generator samples
+ */
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PdfBoxGeneratorOpenhtmltopdfSamples {
+    static PdfGenerator pdfGenerator;
+    static  {
+        try {
+            pdfGenerator = new PdfBoxGenerator();
+            pdfGenerator.init();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * samples generate
+     * 样例生成
+     */
+    @Test
+    public void samplesGenerate() throws Exception {
+        List<File> fileList = FileUtil.list(ResourceUtil.getResourceAbsolutePathByClassPath("samples"));
+        fileList.forEach(f->{
+
+            if(f.getName().equals("tpl.json")) {
+                log.info("samplesGenerate " + f.getName() + "start time：" + new Timestamp(System.currentTimeMillis()));
+                String jsonDataFilePath = null;
+                try {
+                    jsonDataFilePath = ResourceUtil.getResourceAbsolutePathByClassPath(f.getParent() + "/data.json");
+                    JsonNode jsonDataNode = JsonSchemaValidator.getJsonNodeFromFile(jsonDataFilePath);
+                    //依据单个构建配置生成PDF
+                    //generate pdf by element
+                    JSONObject jsonData = (JSONObject) JSONObject.parse(jsonDataNode.toString());
+
+                    String outputFile = "";
+                    String outputFileClass = ResourceUtil.getResourceAbsolutePathByClassPath("");
+
+                    // 生成pdf路径
+                    // generate pdf path
+                    outputFile = (outputFile == null || outputFile.equals("")) ? new File(outputFileClass)
+                            .getParentFile().getParent()
+                            + "/tmp/"
+                            + f.getParentFile().getName() + "_sample_" + System.currentTimeMillis() + ".pdf" : outputFile;
+
+                    //数据参数可以为空
+                    pdfGenerator.generatePdfFileByTplConfigFile(f.getAbsolutePath(), jsonData, outputFile);
+
+                    log.info("samplesGenerate "+ f.getName() +  " end time：" + new Timestamp(System.currentTimeMillis()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+
+
+
+
+
+    }
+
+    
+}
