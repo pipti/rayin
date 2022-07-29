15b1c0bbc7815a3e20713bfcb830ad77626764ae
六耳 <liuercode@163.com>
2022-07-29 17:35:48 +0800

    上传错误移除

--- rayin-design-vue/pluginsCopyToNodeModule.bat
+++
@@ -1,8 +0,0 @@
-copy ./public/tinymce/plugins/mark ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/newblock ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/pdfmedia ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/visualthymeleaf ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/lineheight ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/indent2em ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/pdfcon ./node_modules/tinymce/plugins
-copy ./public/tinymce/plugins/absdiv ./node_modules/tinymce/plugins
