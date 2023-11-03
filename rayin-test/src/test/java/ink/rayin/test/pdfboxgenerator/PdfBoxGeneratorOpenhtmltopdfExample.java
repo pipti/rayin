45f64eacd92428323ec289f16f58915195489089
六耳 <liuercode@163.com>
2023-11-03 17:12:57 +0800

    增加图片处理方法

--- rayin-test/src/test/java/ink/rayin/test/pdfboxgenerator/PdfBoxGeneratorOpenhtmltopdfExample.java
+++ rayin-test/src/test/java/ink/rayin/test/pdfboxgenerator/PdfBoxGeneratorOpenhtmltopdfExample.java
@@ -405,25 +405,4 @@ public class PdfBoxGeneratorOpenhtmltopdfExample {
         log.info("exp13BookmarkTest duration：" +  watch.getTime() + "ms");
     }
 
-    @Test
-    @Order(15)
-    public void exp15BookmarkTest() throws Exception {
-        String outputFileClass = ResourceUtil.getResourceAbsolutePathByClassPath("./");
-//        String jsonDataFilePath = ResourceUtil.getResourceAbsolutePathByClassPath("examples/example13/data.json");
-//        JsonNode jsonDataNode = JsonSchemaValidator.getJsonNodeFromFile(jsonDataFilePath);
-
-//        JSONObject jsonData = JSONObject.parseObject(jsonDataNode.toString());
-
-        String outputFile = new File(outputFileClass)
-                .getParentFile().getParent()
-                + "/tmp/"
-                + "example15_openhtmltopdf_"+System.currentTimeMillis() + ".pdf";
-
-        StopWatch watch = StopWatch.createStarted();
-        //数据参数可以为空
-        //pdfGenerator.generatePdfFileByHtmlAndData(ResourceUtil.getResourceAbsolutePathByClassPath("examples/example13/element1.html"), null, outputFile);
-        pdfGenerator.generatePdfFileByTplConfigFile(ResourceUtil.getResourceAbsolutePathByClassPath("examples/example15/东上U途RPA产品帮助文档_1667296809750tpl.json"),null,outputFile);
-        watch.stop();
-        log.info("exp15BookmarkTest duration：" +  watch.getTime() + "ms");
-    }
 }
